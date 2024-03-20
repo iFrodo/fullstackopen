f5ab96ebc80706f848f739f20e46f432a5c4ef7e
iFrodo <igor@azinet.ru>
2024-03-20 17:40:15 +0300

    2.13

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,22 +1,63 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import contactsService from './services/Contacts.js'
 
+
+const Filter = (props) => {
+  return (
+    <>
+      filter show with: <input value={props.newFilter} onChange={props.onChangge} />
+    </>
+  )
+}
+const AddNewPerson = (props) => {
+  console.log(props)
+  return (
+    <>
+      <div>name: <input value={props.newName} onChange={props.onChangeName} /></div>
+      <div>number: <input value={props.newPhone} onChange={props.onChangePhone} type="tel" /></div>
+    </>
+  )
+}
+const Persons = (props) => {
+  return (
+    <>
+      {props.filteredPersons.map((person) => <p key={person.id}>{person.name} {person.phone}</p>)}
+    </>
+  )
+}
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('+7')
   const [newFilter, setNewFilter] = useState(' ')
   const [filter, setFilter] = useState('true')
 
+  useEffect(() => {
+    console.log('effect')
+    contactsService.getAll()
+      .then(response => {
+        console.log(response)
+        setPersons(response)
+      })
+  }, [])
   const onSubmitForm = (e) => {
     e.preventDefault()
     const newPerson = {
-      id: persons.length + 1,
       name: newName,
       phone: newPhone
     }
-    persons.find(person => person.name === newName) ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(newPerson));
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+
+      contactsService.create(newPerson)
+        .then(result => {
+          console.log(result)
+          setPersons(persons.concat(result));
+        })
+
+    }
+
 
   }
   const onChangeName = (e) => {
@@ -37,19 +78,21 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      filter show with: <input value={newFilter} onChange={onChangeFilter} />
+      <Filter newFilter={newFilter} onChangge={onChangeFilter} />
+
       <form onSubmit={onSubmitForm}>
         <div>
           <h2>add a new</h2>
-          name: <input value={newName} onChange={onChangeName} />
-          <div>number: <input value={newPhone} onChange={onChangePhone} type="tel" pattern="\+7\-[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}" /></div>
+          <AddNewPerson newName={newName} onChangeName={onChangeName} newPhone={newPhone} onChangePhone={onChangePhone} />
+
+
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {filteredPersons.map((person) => <p key={person.id}>{person.name} {person.phone}</p>)}
+      <Persons filteredPersons={filteredPersons} />
     </div>
   )
 }
