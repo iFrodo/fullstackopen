d7f96ff39ec53cf6543b03e91d9241d3e36f8dee
iFrodo <igor@azinet.ru>
2024-04-05 17:05:55 +0300

    tests

--- part4/notes/express/controllers/notes.js
+++ part4/notes/express/controllers/notes.js
@@ -10,7 +10,6 @@ notesRouter.get('/', async (req, res, error) => {
             }
         }
     ).catch(error => next(error))
-
 })
 notesRouter.get('/:id', async (req, res, next) => {
     const note = await Note.findById(req.params.id)
@@ -22,7 +21,6 @@ notesRouter.get('/:id', async (req, res, next) => {
         .catch((error) => {
             next(error)
         })
-
 })
 notesRouter.post('/', async (req, res) => {
     const note = new Note({
@@ -32,16 +30,6 @@ notesRouter.post('/', async (req, res) => {
     const savedNote = await note.save();
     res.json(savedNote);
 })
-
-
-// const noteId = req.params.id; //айдишка
-// const note = notes.find(n => n.id === noteId); // ищем в массиве нотес запись с айдишкой придешдшей в запросе
-// if (!note) {
-//     return res.status(404).json({ error: 'Note not found' });
-// }
-// const newNote = { ...note, important: !note.important };  // потрошим спредом запись и меняем свойство на обратное
-// notes = notes.map(n => (n.id === noteId ? newNote : n)); // берем массив notes  = notes.map (элемент массива notes => элемент ID === пришедшему в запросе ? подставь новую запись , или оставь старую)
-// res.json(newNote);
 notesRouter.put('/:id', async (req, res) => {
     const { content, important } = req.body
     const updatedNote = await Note.findByIdAndUpdate(
@@ -54,8 +42,6 @@ notesRouter.put('/:id', async (req, res) => {
     }
 });
 notesRouter.delete('/:id', async (req, res, next) => {
-    // notes = notes.filter(el => el.id !== req.params.id)
-    // res.json(notes)
     await Note.findByIdAndDelete(req.params.id)
         .then(async (result) => {
             res.json(await Note.find({}))
