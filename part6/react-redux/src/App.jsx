31f28582ccb004cd5c2dda0c266ac43821b90b69
iFrodo <igor@azinet.ru>
2024-09-03 17:29:00 +0300

    refactoring actionCreators,components

---
+++ part6/react-redux/src/App.jsx
@@ -0,0 +1,13 @@
+import Notes from "./components/Notes"
+import NewNoteForm from "./components/NewNoteForm"
+
+const App = () => {
+  return(
+    <div>
+      <NewNoteForm />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
