7d599dc34e53582054d2439daf7c265af2cbf7c3
iFrodo <igor@azinet.ru>
2024-09-17 12:44:53 +0300

    useContext

--- part6/counter-useReducer/src/main.jsx
+++ part6/counter-useReducer/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
+import { CounterContextProvider } from './CounterContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <CounterContextProvider>
     <App />
-  </StrictMode>,
+
+  </CounterContextProvider>
 )
