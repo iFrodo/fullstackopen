4bd84c94b0ba28c3f84c0e9c45e3531a8c745c73
iFrodo <igor@azinet.ru>
2024-10-29 14:14:58 +0300

    named queries

---
+++ part8/react-gql-apollo/src/Person.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const FIND_PERSON = gql`
+    query findPersonByName($nameToSearch: String!) {
+        findPerson(name: $nameToSearch) {
+            name
+            phone
+            id
+            address {
+                street
+                city
+            }
+        }
+    }
+`
+
+const Person = ({ person, onClose }) => {
+    return (
+        <div>
+            <h2>{person.name}</h2>
+            <div>
+                {person.address.street} {person.address.city}
+            </div>
+            <div>{person.phone}</div>
+            <button onClick={onClose}>close</button>
+        </div>
+    )
+}
+
+const Persons = ({ persons }) => {
+// нам приходят persons из app, мы рендерим каждый элемент persons и добавляем обработчик на кнопку который сетит имя в состояние
+// в запросе  указано условие что при nameToSearch = null не делать запрос
+// когда обновляется состояние это вызывает ререндер компоненты, и срабатывает запрос и условие которе возвращает компонент Person который отображает подробную информацию
+// ей так же передается обработчик для зануления состояния которое снова вызывает ререндер и отображается список всех persons
+
+    const [nameToSearch, setNameToSearch] = useState(null)
+    const result = useQuery(FIND_PERSON, {
+        variables: { nameToSearch },
+        skip: !nameToSearch,
+    })
+    console.log(result)
+
+
+    if (nameToSearch && result.data) {
+        return (
+            <Person
+                person={result.data.findPerson}
+                onClose={() => setNameToSearch(null)}
+            />
+        )
+    }
+
+    return (
+        <div>
+            <h2>Persons</h2>
+            {persons.map((p) => (
+                <div key={p.name}>
+                    {p.name} {p.phone}
+
+                    <button onClick={() => setNameToSearch(p.name)}>
+                        show address
+                    </button>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Persons
